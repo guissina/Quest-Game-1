787d931d0747a2337cc459568e8a5ffcd9c1302b
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-03 20:26:42 -0300

    feat: add question services and interfaces

--- server/quest/src/main/java/com/Quest/quest/interfaces/IQuestionServices.java
+++ server/quest/src/main/java/com/Quest/quest/interfaces/IQuestionServices.java
@@ -1,5 +1,14 @@
 package com.Quest.quest.interfaces;
 
+import com.Quest.quest.dto.Question.QuestionCreateDTO;
+import com.Quest.quest.dto.Question.QuestionResponseDTO;
+import com.Quest.quest.models.Question;
+
 public interface IQuestionServices {
 
+    Question findQuestionById(long id);
+
+    QuestionResponseDTO create(QuestionCreateDTO questionCreateDTO);
+
+    QuestionResponseDTO findById(long id);
 }
--- server/quest/src/main/java/com/Quest/quest/mappers/QuestionMapper.java
+++ server/quest/src/main/java/com/Quest/quest/mappers/QuestionMapper.java
@@ -3,22 +3,23 @@ package com.Quest.quest.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
+import com.Quest.quest.dto.Question.QuestionCreateDTO;
 import com.Quest.quest.dto.Question.QuestionResponseDTO;
+import com.Quest.quest.dto.Question.QuestionUpdateDTO;
 import com.Quest.quest.models.Question;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
+
     QuestionResponseDTO toQuestionResponseDTO(Question question);
 
     Question toEntity(QuestionResponseDTO questionResponseDTO);
 
+    Question toEntity(QuestionCreateDTO questionCreateDTO);
+
+    Question toEntity(QuestionUpdateDTO questionUpdateDTO);
+
     List<QuestionResponseDTO> toQuestionResponseDTOs(List<Question> questions);
 
-    @Mapping(target = "id", ignore = true)
-    Question toEntityWithoutId(QuestionResponseDTO questionResponseDTO);
-
-    @Mapping(target = "id", ignore = true)
-    Question toEntityWithoutId(Question question);
 }
