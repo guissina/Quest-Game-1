ab6c8822670139ac3dcfbc12e0ba939af00eab73
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-03 15:40:21 -0300

    feat: add PlayerUpdateDTO

---
+++ server/quest/src/main/java/com/Quest/quest/dto/Player/PlayerUpdateDTO.java
@@ -0,0 +1,41 @@
+package com.Quest.quest.dto.Player;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PlayerUpdateDTO {
+    private long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Size(min = 5, max = 50, message = "Email must be between 5 and 50 characters")
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
