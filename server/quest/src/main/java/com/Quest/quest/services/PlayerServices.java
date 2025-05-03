98b55f91b13ebdd99948e11ae2e486d8db50d890
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-03 19:17:48 -0300

    feat: add player services

---
+++ server/quest/src/main/java/com/Quest/quest/services/PlayerServices.java
@@ -0,0 +1,63 @@
+package com.Quest.quest.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Quest.quest.dto.Player.PlayerResponseDTO;
+import com.Quest.quest.dto.Player.PlayerUpdateDTO;
+import com.Quest.quest.interfaces.IPlayerServices;
+import com.Quest.quest.mappers.PlayerMapper;
+import com.Quest.quest.models.Player;
+import com.Quest.quest.repositories.PlayerRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class PlayerServices implements IPlayerServices {
+
+    private final PlayerMapper playerMapper;
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerServices(PlayerMapper playerMapper, PlayerRepository playerRepository) {
+        this.playerMapper = playerMapper;
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public Player findPlayerById(long id) {
+        return playerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Player not found with id: " + id));
+    }
+
+    @Override
+    public PlayerResponseDTO findById(long id) {
+        Player player = findPlayerById(id);
+        return playerMapper.toPlayerResponseDTO(player);
+    }
+
+    @Override
+    public PlayerResponseDTO update(PlayerUpdateDTO playerUpdateDTO) {
+        Player currentPlayer = findPlayerById(playerUpdateDTO.getId());
+
+        if (!currentPlayer.getEmail().equals(playerUpdateDTO.getEmail()))
+            currentPlayer.setEmail(playerUpdateDTO.getEmail());
+
+        if (!currentPlayer.getName().equals(playerUpdateDTO.getName()))
+            currentPlayer.setName(playerUpdateDTO.getName());
+
+        Player player = playerMapper.toEntity(playerUpdateDTO);
+
+        Player updatedPlayer = playerRepository.save(player);
+        return playerMapper.toPlayerResponseDTO(updatedPlayer);
+    }
+
+    
+
+    @Override
+    public void deletePlayerById(long id) {
+        Player player = findPlayerById(id);
+        playerRepository.delete(player);
+    }
+
+}
