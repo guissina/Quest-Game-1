f4587a07cb9e8b7f633397cca1413e2f3dff9813
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-05 18:17:13 -0300

    feat add SpecialCard Mapper tracking

--- server/quest/src/main/java/com/Quest/quest/mappers/SpecialCardMapper.java
+++ server/quest/src/main/java/com/Quest/quest/mappers/SpecialCardMapper.java
@@ -3,6 +3,7 @@ package com.Quest.quest.mappers;
 import java.util.List;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.Quest.quest.dto.SpecialCard.SpecialCardCreateDTO;
 import com.Quest.quest.dto.SpecialCard.SpecialCardResponseDTO;
@@ -13,8 +14,11 @@ public interface SpecialCardMapper {
 
     SpecialCardResponseDTO toSpecialCardResponseDTO(SpecialCard specialCard);
 
+    @Mapping(target = "specialtyType", source = "specialtyType")
     SpecialCard toEntity(SpecialCardResponseDTO specialCardResponseDTO);
 
+    @Mapping(target = "specialtyType", source = "specialtyType")
+    @Mapping(target = "id", ignore = true)
     SpecialCard toEntity(SpecialCardCreateDTO specialCardCreateDTO);
 
     List<SpecialCardResponseDTO> toSpecialCardResponseDTOs(List<SpecialCard> specialCards);
