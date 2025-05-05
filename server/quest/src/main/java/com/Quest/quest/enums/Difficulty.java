a4a815f1f1acb970c8cf8a035c5652b986b4991d
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-05 01:47:10 -0300

    feat: add difficulty enum

--- server/quest/src/main/java/com/Quest/quest/enums/Difficulty.java
+++ server/quest/src/main/java/com/Quest/quest/enums/Difficulty.java
@@ -1,5 +1,17 @@
 package com.Quest.quest.enums;
 
 public enum Difficulty {
-    
+    EASY("Facil"),
+    MEDIUM("Medio"),
+    HARD("Dificil");
+
+    private final String description;
+
+    Difficulty(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
