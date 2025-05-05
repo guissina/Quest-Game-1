8096a1f7558e2aa80a470c36b020fbb4ffccd862
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-05 02:22:43 -0300

    feat: add Theme Controller, Services, Repository, Interface

---
+++ server/quest/src/main/java/com/Quest/quest/controllers/ThemeController.java
@@ -0,0 +1,67 @@
+package com.Quest.quest.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Quest.quest.dto.Theme.ThemeCreateDTO;
+import com.Quest.quest.dto.Theme.ThemeResponseDTO;
+import com.Quest.quest.dto.Theme.ThemeUpdateDTO;
+import com.Quest.quest.services.ThemeServices;
+
+@RestController
+@RequestMapping("/themes")
+public class ThemeController {
+    private final ThemeServices themeServices;
+
+    @Autowired
+    public ThemeController(ThemeServices themeServices) {
+        this.themeServices = themeServices;
+    }
+
+    @PostMapping
+    public ResponseEntity<ThemeResponseDTO> createTheme(@RequestBody ThemeCreateDTO themeCreateDTO) {
+        ThemeResponseDTO createdTheme = themeServices.create(themeCreateDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTheme);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ThemeResponseDTO> updateTheme(@RequestBody ThemeUpdateDTO themeUpdateDTO) {
+        ThemeResponseDTO updatedTheme = themeServices.update(themeUpdateDTO);
+        return ResponseEntity.ok(updatedTheme);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ThemeResponseDTO> getThemeById(@RequestBody ThemeUpdateDTO themeUpdateDTO) {
+        ThemeResponseDTO theme = themeServices.findById(themeUpdateDTO.getId());
+        return ResponseEntity.ok(theme);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ThemeResponseDTO>> getAllThemes() {
+        List<ThemeResponseDTO> themes = themeServices.findAll();
+        return ResponseEntity.ok(themes);
+    }
+
+    @GetMapping("/findByName")
+    public ResponseEntity<ThemeResponseDTO> getThemeByName(@RequestBody ThemeUpdateDTO themeUpdateDTO) {
+        ThemeResponseDTO theme = themeServices.findById(themeUpdateDTO.getId());
+        return ResponseEntity.ok(theme);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTheme(@RequestBody ThemeUpdateDTO themeUpdateDTO) {
+        themeServices.delete(themeUpdateDTO.getId());
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ server/quest/src/main/java/com/Quest/quest/repositories/ThemeRepository.java
@@ -0,0 +1,9 @@
+package com.Quest.quest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Quest.quest.models.Theme;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+
+}
