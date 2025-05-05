c0ee58306c6594f63b0995315d8399da47da64a6
Gabriel <gabrielg-cruz@hotmail.com>
2025-05-05 00:28:58 -0300

    fix: wrong http process in update method

--- server/quest/src/main/java/com/Quest/quest/controllers/QuestionController.java
+++ server/quest/src/main/java/com/Quest/quest/controllers/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.Quest.quest.dto.Question.QuestionCreateDTO;
 import com.Quest.quest.dto.Question.QuestionResponseDTO;
@@ -14,6 +16,7 @@ import com.Quest.quest.dto.Question.QuestionUpdateDTO;
 import com.Quest.quest.services.QuestionServices;
 
 @Mapper(componentModel = "spring")
+@RequestMapping("/questions")
 public class QuestionController {
     private final QuestionServices questionServices;
 
@@ -28,7 +31,7 @@ public class QuestionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
     }
 
-    @PostMapping("/update")
+    @PutMapping("/{id}")
     public ResponseEntity<QuestionResponseDTO> updateQuestion(@RequestBody QuestionUpdateDTO questionUpdateDTO) {
         QuestionResponseDTO updatedQuestion = questionServices.update(questionUpdateDTO);
         return ResponseEntity.ok(updatedQuestion);
