751c71c4a44ea61abf7b26a8cf5f25d4e0bec467
Luiz Henrique de Santana <dev.luizhenrique@outlook.com>
2025-06-12 20:59:14 -0300

    feat: allow CORS origins via environment variable

--- server/src/main/java/com/quest/config/CorsConfig.java
+++ server/src/main/java/com/quest/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.quest.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,18 +11,23 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
+
+    private final CorsProperties corsProps;
+
+    @Autowired
+    public CorsConfig(CorsProperties corsProps) {
+        this.corsProps = corsProps;
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-            "http://localhost:3000",
-            "http://localhost:5173", 
-            "http://192.168.15.22:5173/",
-            "https://moria-desbravadores-bice.vercel.app"
-        ));
+
+        configuration.setAllowedOrigins(corsProps.getAllowedOrigins());
+        configuration.setAllowedOriginPatterns(corsProps.getAllowedOrigins());
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
--- server/src/main/java/com/quest/config/websocket/WebSocketConfig.java
+++ server/src/main/java/com/quest/config/websocket/WebSocketConfig.java
@@ -1,7 +1,8 @@
 package com.quest.config.websocket;
 
+import com.quest.config.CorsProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,17 +12,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final CorsProperties corsProps;
+
+    @Autowired
+    public WebSocketConfig(CorsProperties corsProps) {
+        this.corsProps = corsProps;
+    }
+
     @Override
-    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                // .setAllowedOriginPatterns("*"); // USE THIS TO USE WITH POSTMAN
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns(corsProps.getAllowedOrigins().toArray(new String[0]))
                 .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app")
                 .enableSimpleBroker("/topic", "/queue");
         registry.setUserDestinationPrefix("/user");
